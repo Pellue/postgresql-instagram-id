0f0a348533071b088fe254c9835a7c5fe9b6a8d8
geun <geunbaelee@gmail.com>
2017-05-23 17:24:09 +0900

    fix knex connection

--- dist/shardable.js
+++ dist/shardable.js
@@ -22,17 +22,19 @@ var Shardable = function () {
 	function Shardable(knex) {
 		(0, _classCallCheck3.default)(this, Shardable);
 
-		this.knex = knex;
+		this._knex = knex;
 	}
 
 	(0, _createClass3.default)(Shardable, [{
 		key: 'createNextIdFunction',
 		value: function createNextIdFunction() {
+			var _this = this;
+
 			var schemaName = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'public';
 
 			return new _bluebird2.default(function (resolve, reject) {
 				var sql = '\n\t\tCREATE OR REPLACE FUNCTION ' + schemaName + '.next_id(In seq_name regclass, set_shard_id int, OUT result bigint) AS $$\n\t\tDECLARE\n\t\t\tour_epoch bigint := 1314220021721;\n\t\t\tseq_id bigint;\n\t\t\tnow_millis bigint;\n\t\t\tshard_id int := set_shard_id;\n\t\t\tmod_key bigint := 1024;\n\t\tBEGIN\n\t\t\tSELECT nextval(seq_name) % mod_key INTO seq_id;\n\t\t\tSELECT FLOOR(EXTRACT(EPOCH FROM clock_timestamp()) * 1000) INTO now_millis;\n\t\t\tresult := (now_millis - our_epoch) << 23;\n\t\t\tresult := result | (shard_id << 10);\n\t\t\tresult := result | (seq_id);\n\t\tEND\n\t\t$$ LANGUAGE PLPGSQL;\n\t';
-				knex.raw(sql).then(function (results) {
+				_this.knex.raw(sql).then(function (results) {
 					return resolve(results);
 				}).catch(function (err) {
 					return reject(err);
@@ -42,11 +44,14 @@ var Shardable = function () {
 	}, {
 		key: 'dropNextIdFunction',
 		value: function dropNextIdFunction() {
+			var _this2 = this;
+
 			var schemaName = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'public';
+
 
 			return new _bluebird2.default(function (resolve, reject) {
 				var sql = 'DROP FUNCTION ' + schemaName + '.next_id(In seq_name regclass, set_shard_id int, OUT result bigint);';
-				knex.raw(sql).then(function (results) {
+				_this2.knex.raw(sql).then(function (results) {
 					return resolve(results);
 				}).catch(function (err) {
 					return reject(err);
@@ -56,14 +61,16 @@ var Shardable = function () {
 	}, {
 		key: 'setShardPrimaryKey',
 		value: function setShardPrimaryKey(tableName) {
+			var _this3 = this;
+
 			var owner = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
 			var shardId = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 1;
 
 			return new _bluebird2.default(function (resolve, reject) {
-				var user = owner === null ? knex.client.config.connection.user : owner;
+				var user = owner === null ? _this3.knex.client.config.connection.user : owner;
 
 				var sql = '\n\t\t\tCREATE SEQUENCE ' + tableName + '_id_seq\n\t\t\t  INCREMENT 1\n\t\t\t  MINVALUE 1\n\t\t\t  MAXVALUE 9223372036854775807\n\t\t\t  START 1\n\t\t\t  CACHE 1;\n\t\t\tALTER TABLE ' + tableName + '_id_seq\n\t\t\t  OWNER TO ' + user + ';\n\t\n\t\t\tALTER TABLE ' + tableName + ' ADD CONSTRAINT ' + tableName + '_pkey PRIMARY KEY(id);\n\t\t\tALTER TABLE ' + tableName + ' ALTER COLUMN id SET DEFAULT next_id(\'' + tableName + '_id_seq\'::regclass, ' + shardId + ');\n\t\t\t';
-				knex.raw(sql).then(function (results) {
+				_this3.knex.raw(sql).then(function (results) {
 					return resolve(results);
 				}).catch(function (err) {
 					return reject(err);
@@ -73,9 +80,11 @@ var Shardable = function () {
 	}, {
 		key: 'dropSequence',
 		value: function dropSequence(tableName) {
+			var _this4 = this;
+
 			return new _bluebird2.default(function (resolve, reject) {
 				var sql = 'DROP SEQUENCE ' + tableName + '_id_seq;';
-				knex.raw(sql).then(function (results) {
+				_this4.knex.raw(sql).then(function (results) {
 					return resolve(results);
 				}).catch(function (err) {
 					return reject(err);
@@ -84,11 +93,8 @@ var Shardable = function () {
 		}
 	}, {
 		key: 'knex',
-		set: function set(knex) {
-			this.knex = knex;
-		},
 		get: function get() {
-			return this.knex;
+			return this._knex;
 		}
 	}]);
 	return Shardable;
